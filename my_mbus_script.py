cb38a075ba62f6cbacc25b8da311973eae622b3b
GDSFR <lelikov-ilya@inbox.ru>
2023-11-21 20:43:16 +0300

    serial or minimalbodbus

--- my_mbus_script.py
+++ my_mbus_script.py
@@ -1,40 +1,30 @@
 import serial
+import minimalmodbus
 
-
-def request_construct(baudrate, slave_id, to_reset=None):
+def request_construct(slave_id, to_reset=None):
     a = 1
     b = 0
     if to_reset:
         return a
-    else:
-        return b
-
-
-def response_check(response):
-    response_good = b'bbbbb'
-    if response == response_good:
-        return True
-    else:
-        return False
-
+    return b
 
 def main():
+    port = '/dev/ttyACM0'
     baudrates = [110, 300, 1200, 2400, 9600, 19200, 28800, 38400, 57600, 115200, 230400, 460800, 921600]
     slaves_num = 20
     found = 0
     need = 1
-    request = b'aaaaa'
-    ser = serial.Serial('/dev/ttyACM0', baudrates[0], timeout=0.3)
+    response_good = b'bbbbb'
+    timeout = 0.2
 
-    for i in range(1, slaves_num + 1):  # 1 --- 20
+    for i in range(1, slaves_num + 1):  # 1 --- 20 SLAVES
         for j in range(len(baudrates)):
-            ser.write(request_construct(
-                baudrate=baudrates[j],
-                slave_id=i
-            ))
-            if response_check(response=ser.read()):
+#            ser = serial.Serial(port, baudrates[j], timeout=timeout)
+#            ser = minimalmodbus.Instrument(port, i, baudrates)
+            print(f"i - {i}, j = {j}")
+            ser.write(request_construct(slave_id=i))
+            if ser.read() == response_good:
                 request_construct(
-                    baudrate=baudrates[j],
                     slave_id=i,
                     to_reset=True
                 )
